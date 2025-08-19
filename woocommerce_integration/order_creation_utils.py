7d90e90e5bdfd2d0ab1570ccaa95130c36f7d10d
viralkansodiya <viral@fosserp.com>
2025-08-19 15:32:30 +0000

    fix: taxes issue

--- woocommerce_integration/order_creation_utils.py
+++ woocommerce_integration/order_creation_utils.py
@@ -145,6 +145,10 @@ def create_order(order: dict, woocommerce_setup: dict, customer: str):
 
     add_items_to_sales_order(order, sales_order, woocommerce_setup)
 
+    sales_order.flags.ignore_permissions = True
+    sales_order.run_method("set_missing_values")
+    sales_order.run_method("calculate_taxes_and_totals")
+
     sales_order.flags.ignore_mandatory = True
     sales_order.insert()
     sales_order.submit()
@@ -167,24 +171,26 @@ def add_items_to_sales_order(order: dict, sales_order: dict, setup: dict):
                 "warehouse": setup.default_warehouse,
             },
         )
+    
 
-        if ordered_items_tax := flt(line_item.get("total_tax")):
-            add_tax_details(
-                sales_order, ordered_items_tax, "Item Tax", setup.tax_account
-            )
 
-    add_tax_details(
-        sales_order,
-        flt(order.get("shipping_tax")),
-        "Shipping Tax",
-        setup.shipping_tax_account,
-    )
-    add_tax_details(
-        sales_order,
-        flt(order.get("shipping_total")),
-        "Shipping Total",
-        setup.shipping_tax_account,
-    )
+    #     if ordered_items_tax := flt(line_item.get("total_tax")):
+    #         add_tax_details(
+    #             sales_order, ordered_items_tax, "Item Tax", setup.tax_account
+    #         )
+
+    # add_tax_details(
+    #     sales_order,
+    #     flt(order.get("shipping_tax")),
+    #     "Shipping Tax",
+    #     setup.shipping_tax_account,
+    # )
+    # add_tax_details(
+    #     sales_order,
+    #     flt(order.get("shipping_total")),
+    #     "Shipping Total",
+    #     setup.shipping_tax_account,
+    # )
 
 
 def get_item(item_data: dict, setup: dict) -> dict:
